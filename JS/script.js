95cef74c9c202d040027dd3ed44b2a820fcc828d
Bruni12 <49500292+Bruni12@users.noreply.github.com>
2020-01-23 20:06:04 -0700

    Add files via upload
    finished script

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ function Particle(x,y, directionX, directionY, size, color){
     this.x = x,
     this.y = y;
     this.directionX = directionX;
-    this.directionY = directonY;
+    this.directionY = directionY;
     this.size = size;
     this.color = color;
 }
@@ -23,8 +23,47 @@ Particle.prototype.draw = function(){
 }
 // add update method to particle prototype
 Particle.prototype.update = function(){
-    if (this.x + this.siez > canvas.width || this.x - this.size < 0) {
-        this.directionXv = -this.directionX;
+    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+        this.directionX = -this.directionX;
     }
-    
+    if (this.y + this.size > canvas.width || this.y - this.size < 0) {
+        this.directionY = -this.directionY;
+    }
+    this.x += this.directionX;
+    this.y += this.directionY;
+
+    this.draw();
 }
+// create particle array
+function init() {
+    particleArray = [];
+    for (let i =0; i< 100; i++) {
+        let size = Math.random() * 20;
+        let x = Math.random() * (innerWidth - size * 2);
+        let y = Math.random() * (innerHeight - size * 2);
+        let directionX = (Math.random() * .4) - .2;
+        let directionY = (Math.random() * .4) - .2;
+        let color = 'white';
+
+        particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+//  animation loop
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearReact(0,0, innerWidth, innerHeight);
+
+    for (let i=0; i<particpleArray.length; i++) {
+        particleArray[i].update();
+    }
+}
+init();
+animate();
+
+window.addEventListener('resize',
+    function(){
+        canvas.width = innerWidth;
+        canvas.height = innderHeight;
+        init();
+    }
+) 
